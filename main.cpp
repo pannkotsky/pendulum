447fdd8d99f811b5dbd2e8c883f51f12f9810570
Valerii Kovalchuk <kovvalole@gmail.com>
2018-06-12 13:48:03 +0300

    Rework to spring pendulum

--- main.cpp
+++ main.cpp
@@ -11,23 +11,26 @@ const int SCREEN_WIDTH  = 960;
 const int SCREEN_HEIGHT = 720;
 const int PADDING = SCREEN_WIDTH / 24;
 
-const int L = SCREEN_WIDTH / 4 - PADDING * 2; // length of the pendulum
+const int L = SCREEN_HEIGHT / 4 - PADDING; // максимальне відхилення для цілей виведення на екран
 
-const double MASS_MIN = 10.0; // min mass of the body
-const double MASS_MAX = 30.0; // max mass of the body
+const double MASS_MIN = 5.0; // мінімальна маса тіла
+const double MASS_MAX = 10.0; // максимальна маса тіла
 
-const double DEV_MIN = - M_PI / 2; // min deviation in radians
-const double DEV_MAX = M_PI / 2; // max deviation in radians
+const double DEV_MIN = - 7; // мінімальне відхилення
+const double DEV_MAX = 7; // максимальне відхилення
 
-const double STIFFNESS_MIN = 1.0;
-const double STIFFNESS_MAX = 5.0;
+const double STIFFNESS_MIN = 20.0; // мінімальне значення коефіцієнту жорсткості
+const double STIFFNESS_MAX = 80.0; // максимальне значення коефіцієнту жорсткості
 
-const int HANG_POINT[] = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2};
+// координати точки підвісу
+const int HANG_POINT[] = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4 - 60};
+
+const int GRAPH_WIDTH = SCREEN_WIDTH / 2 - PADDING * 4;
 
 
 void logSDLError(const string &msg);
 void drawCircle(SDL_Renderer *renderer, double cx, double cy, double radius);
-void drawPendulum(SDL_Renderer *renderer, double dev, double mass, double stiff);
+void drawSpringPendulum(SDL_Renderer *renderer, double dev, double mass, double stiff);
 void drawText(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Color textColor, int x, int y, const string alignX);
 
 
@@ -75,7 +78,7 @@ int main() {
     SDL_Color currentColor;
 
     int time = 0;
-    double devInit = -1.0; // initial deviation
+    double devInit = -5.0; // initial deviation
     double dev = devInit;
     double mass = (MASS_MAX + MASS_MIN) / 2;
     double stiff = (STIFFNESS_MAX + STIFFNESS_MIN) / 2;
@@ -90,26 +93,32 @@ int main() {
 
         // Draw parameters
         char text[50];
-        sprintf(text, "Маса: %.1f", mass);
+
+        const char *helpText = "Використовуйте клавіші стрілок для зміни параметрів";
+        drawText(renderer, helpFont, helpText, textColor, SCREEN_WIDTH / 2, 10, "center");
+
+        sprintf(text, "Маса: %.1f кг", mass);
         currentColor = activeParam == 0 ? activeTextColor : textColor;
         drawText(renderer, font, text, currentColor, SCREEN_WIDTH / 6, PADDING, "center");
 
-        sprintf(text, "Жорсткість: %.1f", stiff);
+        sprintf(text, "Жорст.: %.1f Н/м", stiff);
         currentColor = activeParam == 1 ? activeTextColor : textColor;
         drawText(renderer, font, text, currentColor, SCREEN_WIDTH / 2, PADDING, "center");
 
-        sprintf(text, "Поч. зсув: %.1f", devInit);
+        sprintf(text, "Поч. зсув: %.1f м", devInit);
         currentColor = activeParam == 2 ? activeTextColor : textColor;
         drawText(renderer, font, text, currentColor, SCREEN_WIDTH * 5 / 6, PADDING, "center");
 
-        const char *helpText = "Використовуйте клавіші стрілок для зміни параметрів";
-        drawText(renderer, helpFont, helpText, textColor, SCREEN_WIDTH / 2, 100, "center");
-
-        sprintf(text, "Час: %d", time / 1000);
-        drawText(renderer, font, text, textColor, SCREEN_WIDTH / 2, SCREEN_HEIGHT - PADDING - 80, "center");
+        sprintf(text, "Час: %d с", time / 1000);
+        drawText(renderer, font, text, textColor, SCREEN_WIDTH - PADDING, SCREEN_HEIGHT - PADDING - 80, "right");
+        float period = 2 * M_PI * sqrt(mass / stiff);
+        sprintf(text, "Період коливань: %.2f с", period);
+        drawText(renderer, font, text, textColor, PADDING, SCREEN_HEIGHT - PADDING - 120, "left");
+        sprintf(text, "Частота коливань: %.2f rad/с", 2 * M_PI / period);
+        drawText(renderer, font, text, textColor, PADDING, SCREEN_HEIGHT - PADDING - 80, "left");
 
         // Draw pendulum
-        drawPendulum(renderer, dev, mass, stiff);
+        drawSpringPendulum(renderer, dev, mass, stiff);
 
         int x0 = SCREEN_WIDTH / 2 + PADDING;
         // Draw the time axis for graph
@@ -120,12 +129,12 @@ int main() {
         );
         int lastSeconds;
         int lastSecondsPosition;
-        if (time / interval < L * 2) {
+        if (time / interval < GRAPH_WIDTH) {
             lastSeconds = 3;
             lastSecondsPosition = x0 + lastSeconds * 100;
         } else {
             lastSeconds = time / 1000;
-            lastSecondsPosition = x0 + L * 2 + (time % 2) / 10 - (time % 1000) / 10;
+            lastSecondsPosition = x0 + GRAPH_WIDTH + (time % 2) / 10 - (time % 1000) / 10;
         }
 
         if (lastSecondsPosition + 100 <= SCREEN_WIDTH - PADDING) {
@@ -146,7 +155,7 @@ int main() {
         sprintf(text, "%d", lastSeconds - 2);
         drawText(renderer, helpFont, text, textColor, lastSecondsPosition - 200, SCREEN_HEIGHT / 2 + 5, "center");
 
-        drawText(renderer, helpFont, "Час", textColor, SCREEN_WIDTH - PADDING, SCREEN_HEIGHT / 2 - 40, "center");
+        drawText(renderer, helpFont, "Час, с", textColor, SCREEN_WIDTH - PADDING, SCREEN_HEIGHT / 2 - 40, "center");
 
         // Draw the deviation axis for graph
         SDL_RenderDrawLine(
@@ -155,18 +164,18 @@ int main() {
                 SCREEN_WIDTH / 2 + PADDING, SCREEN_HEIGHT / 2 + L + PADDING
         );
 
-        drawText(renderer, helpFont, "Зсув", textColor, x0 - 10, SCREEN_HEIGHT / 2 - L - PADDING, "right");
+        drawText(renderer, helpFont, "Зсув, м", textColor, x0 - 10, SCREEN_HEIGHT / 2 - L - PADDING, "right");
         drawCircle(renderer, x0, SCREEN_HEIGHT / 2, 3);
         drawText(renderer, helpFont, "0", textColor, x0 - 10, SCREEN_HEIGHT / 2 + 5, "right");
         drawCircle(renderer, x0, SCREEN_HEIGHT / 2 - L, 3);
-        drawText(renderer, helpFont, "π/2", textColor, x0 - 10, SCREEN_HEIGHT / 2 - L + 5, "right");
+        drawText(renderer, helpFont, "7", textColor, x0 - 10, SCREEN_HEIGHT / 2 - L + 5, "right");
         drawCircle(renderer, x0, SCREEN_HEIGHT / 2 + L, 3);
-        drawText(renderer, helpFont, "-π/2", textColor, x0 - 10, SCREEN_HEIGHT / 2 + L + 5, "right");
+        drawText(renderer, helpFont, "-7", textColor, x0 - 10, SCREEN_HEIGHT / 2 + L + 5, "right");
 
         // Calculate deviation
-        dev = devInit * cos(pow(stiff / mass, -0.5) * (time * 0.01 / interval));
+        dev = devInit * cos(pow(mass / stiff, -0.5) * (time * 0.01 / interval));
         lastPoints.push_back(dev);
-        if (lastPoints.size() > L * 2) {
+        if (lastPoints.size() > GRAPH_WIDTH) {
             lastPoints.pop_front();
         }
 
@@ -176,7 +185,7 @@ int main() {
         double graphX = graphStartX;
         deque<double>::iterator it = lastPoints.begin();
         while (it != lastPoints.end()) {
-            SDL_RenderDrawPoint(renderer, graphX++, graphstartY - L * sin(*it++));
+            SDL_RenderDrawPoint(renderer, graphX++, graphstartY - L * (*it++) / DEV_MAX);
         }
 
         SDL_RenderPresent(renderer);
@@ -193,19 +202,19 @@ int main() {
                 } else if (key == SDL_SCANCODE_UP) {
                     if (activeParam == 0) {
                         if (mass < MASS_MAX) {
-                            mass++;
+                            mass += 0.5;
                             time = 0;
                             lastPoints.clear();
                         }
                     } else if (activeParam == 1) {
                         if (stiff < STIFFNESS_MAX) {
-                            stiff += 0.5;
+                            stiff += 5;
                             time = 0;
                             lastPoints.clear();
                         }
                     } else if (activeParam == 2) {
-                        if (devInit + 0.1 < DEV_MAX) {
-                            devInit += 0.1;
+                        if (devInit < DEV_MAX) {
+                            devInit += 0.5;
                             time = 0;
                             lastPoints.clear();
                         }
@@ -213,19 +222,19 @@ int main() {
                 } else if (key == SDL_SCANCODE_DOWN) {
                     if (activeParam == 0) {
                         if (mass > MASS_MIN) {
-                            mass--;
+                            mass -= 0.5;
                             time = 0;
                             lastPoints.clear();
                         }
                     } else if (activeParam == 1) {
                         if (stiff > STIFFNESS_MIN) {
-                            stiff -= 0.5;
+                            stiff -= 5;
                             time = 0;
                             lastPoints.clear();
                         }
                     } else if (activeParam == 2) {
-                        if (devInit - 0.1 > DEV_MIN) {
-                            devInit -= 0.1;
+                        if (devInit > DEV_MIN) {
+                            devInit -= 0.5;
                             time = 0;
                             lastPoints.clear();
                         }
@@ -267,26 +276,66 @@ void drawCircle(SDL_Renderer *renderer, double cx, double cy, double radius) {
 }
 
 
-void drawPendulum(SDL_Renderer *renderer, double dev, double mass, double stiff) {
+void drawSpringPendulum(SDL_Renderer *renderer, double dev, double mass, double stiff) {
+    int hangPointRadius = 5;
+    // величина відхилення для виведення на екран
+    double devDisplay = L * dev / DEV_MAX;
+    double bodyCenter[] = {SCREEN_WIDTH / 4 , SCREEN_HEIGHT / 2 - devDisplay};
+    double bodyRadius = mass * 3;
+    // кінці пружини
     SDL_RenderDrawLine(
             renderer,
-            SCREEN_WIDTH / 4 - 50, SCREEN_HEIGHT / 2,
-            SCREEN_WIDTH / 4 - stiff * 2, SCREEN_HEIGHT / 2
+            HANG_POINT[0], HANG_POINT[1] + hangPointRadius,
+            HANG_POINT[0], HANG_POINT[1] + hangPointRadius + 10
     );
     SDL_RenderDrawLine(
             renderer,
-            SCREEN_WIDTH / 4 + stiff * 2, SCREEN_HEIGHT / 2,
-            SCREEN_WIDTH / 4 + 50, SCREEN_HEIGHT / 2
+            bodyCenter[0], bodyCenter[1] - bodyRadius - 10,
+            bodyCenter[0], bodyCenter[1] - bodyRadius
     );
-    double x = HANG_POINT[0] + L * sin(dev);
-    double y = HANG_POINT[1] + L * cos(dev);
-    double x1 = HANG_POINT[0] + (L - mass) * sin(dev);
-    double y1 = HANG_POINT[1] + (L - mass) * cos(dev);
-    double x0 = HANG_POINT[0] + stiff * 2 * sin(dev);
-    double y0 = HANG_POINT[1] + stiff * 2 * cos(dev);
-    SDL_RenderDrawLine(renderer, x0, y0, x1, y1);
-    drawCircle(renderer, HANG_POINT[0], HANG_POINT[1], stiff * 2);
-    drawCircle(renderer, x, y, mass);
+    // пружна частина пружини
+    // мінімальна довжина пружини у найбільш стиснутому стані
+    double minSpringLength = SCREEN_HEIGHT / 2 - L - HANG_POINT[1] - hangPointRadius - bodyRadius - 20;
+    int numSpirals = stiff * 0.33;
+    double springLength = minSpringLength + (-devDisplay + L);
+    double spiralLength = springLength / numSpirals;
+    for (int i = 0; i < numSpirals; i++) {
+        SDL_RenderDrawLine(
+                renderer,
+                HANG_POINT[0], HANG_POINT[1] + hangPointRadius + 10 + spiralLength * i,
+                HANG_POINT[0] + 20, HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.25)
+        );
+        SDL_RenderDrawLine(
+                renderer,
+                HANG_POINT[0] + 20, HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.25),
+                HANG_POINT[0], HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.5)
+        );
+        SDL_RenderDrawLine(
+                renderer,
+                HANG_POINT[0], HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.5),
+                HANG_POINT[0] - 20, HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.75)
+        );
+        SDL_RenderDrawLine(
+                renderer,
+                HANG_POINT[0] - 20, HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 0.75),
+                HANG_POINT[0], HANG_POINT[1] + hangPointRadius + 10 + spiralLength * (i + 1)
+        );
+    }
+    // дві лінії стелі
+    SDL_RenderDrawLine(
+            renderer,
+            HANG_POINT[0] - 50, HANG_POINT[1],
+            HANG_POINT[0] - hangPointRadius, HANG_POINT[1]
+    );
+    SDL_RenderDrawLine(
+            renderer,
+            HANG_POINT[0] + hangPointRadius, HANG_POINT[1],
+            HANG_POINT[0] + 50, HANG_POINT[1]
+    );
+    // точка підвісу
+    drawCircle(renderer, HANG_POINT[0], HANG_POINT[1], hangPointRadius);
+    // тіло
+    drawCircle(renderer, bodyCenter[0], bodyCenter[1], bodyRadius);
 }
 
 
